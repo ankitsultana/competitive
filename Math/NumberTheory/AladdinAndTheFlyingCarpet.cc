c23353eb79a91e0daa2ccfa449d6c25b76283308
bk2dcradle <ankitsultana@gmail.com>
2016-04-03 23:15:20 +0530

    update

---
+++ Math/NumberTheory/AladdinAndTheFlyingCarpet.cc
@@ -0,0 +1,141 @@
+/* Author: Ankit Sultana
+ * Problem id: 1341
+ * * * * * * * * * */
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#define LL long long
+#define MAXN 1000003
+
+using namespace std;
+
+bool is_prime[MAXN] = {false};
+vector<LL> primes;
+void sieve_eratosthenes() {
+	for(int i = 0; i < MAXN; i++)	is_prime[i] = true;
+	is_prime[1] = false;
+	for(int i = 2; i*i <= MAXN; i++){
+		if(is_prime[i]){
+			for(int j = i*i; j < MAXN; j += i){
+				is_prime[j] = false;
+			}
+		}
+	}
+	for(int i = 2; i < MAXN; i++)
+		if(is_prime[i])
+			primes.push_back(i);
+}
+
+/* Miller Rabbin,
+ * Complexity: O(k * log2^3(n))
+ * * * * * * * * * * * */
+inline LL multiply(LL a, LL b, const LL &m)
+{
+	a %= m, b %= m;
+	long double res = a;res *= b;
+	LL c = (LL)(res/m);
+	a *= b, a -= c*m, a %= m;
+	if(a < 0)
+		a += m;
+	return a;
+}
+
+inline LL power(LL a, LL b, const LL &m)
+{
+	LL ans = 1;
+	while(b) {
+		if(b & 1) {
+			ans = multiply(ans, a, m);
+		}
+		a = multiply(a, a, m);
+		b >>= 1;
+	}
+	return ans;
+}
+
+// Returns true if p is prime
+inline bool Miller(LL p) {
+	int b[]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
+	if(p < 2)	return false;
+	if(p != 2 && !(p&1))	return false;
+	for(int i = 0; i < 25; i++) {
+		if(p == b[i])	return true;
+		else if(p % b[i] == 0)	return false;
+	}
+	int cnt = 0;
+	LL s = p-1;
+	while(!(s&1)) {
+		s /= 2;
+		cnt++;
+	}
+	LL accuracy = 2, m;
+	for(int i = 0; i < accuracy; i++) {
+		LL a = rand() % (p-1) + 1;
+		m = p;
+		LL x = power(a, s, m);
+		if(x == 1 || x == p-1)	continue;
+		int flag = 0;
+		for(int i = 1; i < cnt; i++) {
+			x = multiply(x, x, m);
+			if(x == 1)	return false;
+			if(x == p-1) {
+				flag = 1;
+				break;
+			}
+		}
+		if(flag)	continue;
+		return false;
+	}
+	return true;
+}
+
+LL count_divisors(LL n)
+{
+	LL ans = 1;
+    for(int i = 0; i < primes.size(); i++) {
+        LL p = primes[i];
+		if(p*p*p > n)	break;
+		LL count = 1;
+		while(n % p == 0) {
+			n /= p;
+			count++;
+		}
+		ans = ans*count;
+	}
+	LL sq = sqrt(double(n));
+	if(Miller(n)) {
+		ans = ans*2;
+	}
+	else if(sq*sq == n && Miller(sq)) {
+		ans *= 3;
+	}
+	else if(n != 1) {
+		ans *= 4;
+	}
+	return ans;
+}
+
+int main() {
+    int t;
+    sieve_eratosthenes();
+    scanf("%d", &t);
+    LL a, b, res;
+    for(int tc = 1; tc <= t; tc++) {
+        scanf("%lld %lld", &a, &b);
+        printf("Case %d: ", tc);
+        if(b >= sqrt(a)) {
+            res = 0;
+        } else {
+            LL s = sqrt(a);
+            res = count_divisors(a)/2;
+            for(LL i = 1; i < b; i++) {
+                if(a % i == 0)
+                    res--;
+            }
+        }
+        printf("%lld\n", res);
+    }
+    return 0;
+}
