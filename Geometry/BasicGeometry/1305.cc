3d28fdcdbbfa17dafb239222dbd4caa820d42d04
bk2dcradle <ankitsultana@gmail.com>
2016-04-18 17:00:37 +0530

    solve

--- Geometry/BasicGeometry/1305.cc
+++ Geometry/BasicGeometry/1305.cc
@@ -1,5 +1,5 @@
 /* Author: Ankit Sultana
- * Problem Name: Calm Down (1072)
+ * Problem Name: Area of a parallelogram (1305)
  * * * * * * * * * */
 #include <iostream>
 #include <iomanip>
@@ -8,16 +8,23 @@
 
 using namespace std;
 
+struct point {
+    LL x, y;
+};
+
+LL p_area(point a, point b, point c) {
+    return abs((a.x - b.x) * c.y + (b.x - c.x) * a.y + (c.x - a.x) * b.y);
+}
+
 int main() {
     int t;
-    double PI = acos(0.0) * 2ll, R;
-    LL n;
     cin >> t;
     for(int tc = 1; tc <= t; tc++) {
-        cin >> R >> n;
-        double theta = PI/double(n);
-        double answer = R*sin(theta)/(1.00l + sin(theta));
-        cout << "Case " << tc << ": " << fixed << setprecision(8) << answer << '\n';
+        point a, b, c, d;
+        cin >> a.x >> a.y >> b.x >> b.y >> c.x >> c.y;
+        d.x = a.x + c.x - b.x;
+        d.y = a.y + c.y - b.y;
+        cout << "Case " << tc << ": " << d.x << ' ' << d.y << ' ' << p_area(a, b, d) << '\n';
     }
     return 0;
 }
